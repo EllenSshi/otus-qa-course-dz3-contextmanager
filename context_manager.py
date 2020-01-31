399a14a001469d5ae331bd690233c039761792fe
eshenko <alexeyeschenko@gmail.com>
2020-01-31 19:27:02 +0300

    print results after running script

--- context_manager.py
+++ context_manager.py
@@ -1,9 +1,8 @@
-import csv
 import json
 import pandas as pd
 
 
-def add_users_and_books_into_result_file():
+def add_users_and_books_into_result_file(rfile):
     results = list()
     with open('users.json', 'r') as users_file:
         users = json.load(users_file)
@@ -12,10 +11,13 @@ def add_users_and_books_into_result_file():
             user_books = books[['Title', 'Author', 'Height']].sample(2).to_dict(orient='records')
             new_user = {"name": user['name'], "gender": user['gender'], "address": user['address'], "books": user_books}
             results.append(new_user)
-    with open('result.json', 'w') as result_file:
-        json.dump(results, result_file, indent=4)
+    with open(rfile, 'w') as r_file:
+        json.dump(results, r_file, indent=4)
 
 
 if __name__ == '__main__':
-    add_users_and_books_into_result_file()
-
+    result_file = 'result.json'
+    add_users_and_books_into_result_file(result_file)
+    with open(result_file, 'r') as result_reader:
+        content = result_reader.read()
+        print(content)
